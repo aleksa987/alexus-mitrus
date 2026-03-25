7d4453c3352e8365b68627e0faf78b7fb8c59272
aleksa987 <aleksailija987@gmail.com>
2026-03-25 16:47:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import os
+import sqlite3
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Zdravo programeri"
+
+if __name__ == '__main__':
+    app.run()
