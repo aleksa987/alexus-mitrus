2ee7737f654f02e9aa256ac4933f10fd4ac093a0
aleksa987 <aleksailija987@gmail.com>
2026-04-22 16:45:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Zdravo programeri"
+    nazivSpiska = "Spisak restorana"
+    spisakRestorana = ["Pastica", "Pica tim", "Has Hub", "Sahara"]
+    return render_template("meni.html, naziv=nazivMeni, spisak=spisakMeni)
+
+@app.route("/restorani/1")
+    def meni():
+        nazivMeni = "Meni Promenada"
+        spisakMeni = ["Sendvici", "Burgeri", "Torte", "Paste"]
+        return render_template("meni.html, naziv=nazivMeni, spisak=spisakMeni)
+
+@app.route("/primer-niz")
+    def niz():
+        nekiNiz = [1,2,3,4,5]
+        return nekiNiz
+
+@app.route("/primer-json")
+    def primeriJson():
+        data = {
+            "message": "This is JSON response",
+            "status": "success"
+        }
+        return (data)
+
+@app.route("/primer-html")
+def primerHTML():
+    data = """<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF 8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible">
+     """
 
 if __name__ == '__main__':
     app.run()
